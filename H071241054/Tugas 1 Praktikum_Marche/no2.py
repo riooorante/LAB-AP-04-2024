77796493fe7c42742ce0d11888abb2d33292f683
Gyerend <gyerendlinta@gmail.com>
2024-09-13 14:08:17 +0800

    hapus directory

--- H071241054/Tugas 1 Praktikum_Marche/no2.py
+++
@@ -1,9 +0,0 @@
-#input
-karakter = str(input("Masukkan karakter: "))
-kalimat = str(input("Masukkan kalimat: "))
-
-#Menggunakan operator "in" untuk memeriksa keberadaan karakter
-pesan = ["tidak ditemukan dalam", "merupakan bagian dari"][karakter in kalimat]
-
-#Menampilkan pesan
-print(f"'{karakter}'{pesan}'{kalimat}'")
