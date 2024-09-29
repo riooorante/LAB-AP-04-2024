60674706f8f3441eda5519b9e2b13ed993a1cf64
Marchegatriani <marchegatrianisude@gmail.com>
2024-09-30 00:03:56 +0800

    feat: Perbaikan tugas praktikum 4 nomor 1 dan nomor 2

--- H071241054/Tugas Praktikum 4/nomor1.py
+++ H071241054/Tugas Praktikum 4/nomor1.py
@@ -6,43 +6,7 @@ def total_skor(kartu):
         skor = skor - 10
     return skor
 
-def draw_card():
-    return random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11])  #1 & 11 mewakili As, dan 10 mewakili J, K, Q
-
-def play_blackjack():
-    print("Welcome to Blackjack!")
-    
-    kartu_pemain = [draw_card()]
-    kartu_dealer = [draw_card()]
-    
-    skor_pemain = total_skor(kartu_pemain)
-    skor_dealer = total_skor(kartu_dealer)
-    
-    print(f"Kartu anda sekarang adalah: {skor_pemain}")
-
-    #pemain mengambil kartu                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
-    while True:
-        pilihan = input("Apakah masih akan mengambil kartu? (y/n): ").lower()
-        if pilihan == 'y':
-            kartu_pemain.append(draw_card())
-            skor_pemain = total_skor(kartu_pemain)
-            print(f"Kartu anda sekarang adalah: {skor_pemain}")
-            if skor_pemain > 21:
-                print("Anda kalah, kartu anda melebihi 21.")
-        elif pilihan == 'n':
-            break
-        else:
-            print("Pilihan tidak valid, silakan coba lagi.")
-
-    print(f"Kartu dealer adalah: {skor_dealer}")
-    
-    #dealer mengambil kartu hingga mencapai 17 atau lebih
-    while skor_dealer < 17:
-        kartu_dealer.append(draw_card())
-        skor_dealer = total_skor(kartu_dealer)
-        print(f"Kartu dealer sekarang adalah: {skor_dealer}")
-
-    #menentukan hasil permainan
+def penentuan(skor_dealer, skor_pemain):
     if skor_dealer > 21:
         print("Anda menang, dealer melebihi 21.")
     elif skor_pemain > skor_dealer:
@@ -52,4 +16,36 @@ def play_blackjack():
     else:
         print("Seri!")
 
-play_blackjack()
+#memainkan permainan blackjack
+print("Welcome to Blackjack!")
+kartu_pemain = [random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11])]       
+kartu_dealer = [random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11])]
+
+skor_pemain = total_skor(kartu_pemain)
+skor_dealer = total_skor(kartu_dealer)
+
+print(f"Kartu anda sekarang adalah: {skor_pemain}")
+
+while True:
+    pilihan = input("Apakah masih akan mengambil kartu? (y/n): ").lower()
+    if pilihan == 'y':
+        kartu_pemain.append(random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]))
+        skor_pemain = total_skor(kartu_pemain)
+        print(f"Kartu anda sekarang adalah: {skor_pemain}")
+        if skor_pemain > 21:
+            print("Anda kalah, kartu anda melebihi 21.")
+            exit()
+    else:
+        if pilihan == 'n':
+            break
+        else:
+            print("Input tidak valid. Masukkan (y/n) untuk memilih kartu!")
+
+print(f"Kartu dealer adalah: {skor_dealer}")
+
+while skor_dealer < 17:
+    kartu_dealer.append(random.choice([2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]))
+    skor_dealer = total_skor(kartu_dealer)
+    print(f"Kartu dealer sekarang adalah: {skor_dealer}")
+
+penentuan(skor_dealer, skor_pemain)
--- H071241054/Tugas Praktikum 4/nomor2.py
+++ H071241054/Tugas Praktikum 4/nomor2.py
@@ -3,40 +3,36 @@ def hitung_total_jarak(langkah):
     global total_jarak
     total_jarak += langkah
 
-def mencari_harta_karun():
-    bahaya = False
-    
-    while True:
-        try:
-            langkah = int(input("Masukkan langkah (meter) atau 0 untuk selesai: "))
-
-            if langkah < 0:
-                print("Input tidak valid. Harap masukkan bilangan bulat positif!")
-                continue
-            if langkah == 0:
-                break
-            if langkah > 20:
-                bahaya = True
-                print("Hati-hati langkah berbahaya!")
-
-            hitung_total_jarak(langkah)
-
-        except ValueError:
-            print("Input tidak valid. Harap masukkan bilangan bulat positif!")
-
-    print(f"Total jarak: {total_jarak}")
-
 def ada_bahaya_keputusan(total_jarak):
-    mencari_harta_karun()
     if total_jarak == 50:
-        print("Ada bahaya: Tidak")
-        print("Keputusan: Aman! Kamu tepat menemukan harta karun dan menang!")
+        bahaya = "Tidak"
+        keputusan = "Aman! Kamu tepat menemukan harta karun dan menang!"
+    elif total_jarak < 50:
+        bahaya = "-"
+        keputusan = "Tidak menemukan harta karun. Coba lagi!"
     else:
-        if total_jarak < 50:
-            print("Ada bahaya: Ya")
-            print("Keputusan: Tidak aman untuk menggali harta karun. Coba lagi!")
-        else:
-            print("Ada bahaya: Ya")
-            print("Keputusan: Tidak aman untuk menggali harta karun. Coba lagi!")
+        bahaya = "Ya"
+        keputusan = "Tidak aman untuk menggali harta karun. Coba lagi!"
 
+    print(f"Ada bahaya: {bahaya}")
+    print(f"Keputusan: {keputusan}")
+
+while True:
+    try:
+        langkah = int(input("Masukkan langkah (meter) atau 0 untuk selesai: "))
+
+        if langkah < 0:
+            print("Input tidak valid. Harap masukkan bilangan bulat positif!")
+            continue
+        if langkah == 0:
+            break
+        if langkah > 20:
+            print("Hati-hati langkah anda berbahaya!")
+
+        hitung_total_jarak(langkah)
+
+    except ValueError:
+        print("Input tidak valid. Harap masukkan bilangan bulat positif!")
+    
+print(f"Total jarak: {total_jarak} meter")
 ada_bahaya_keputusan(total_jarak)
